b7868210f10dd0759edd72bd65151a19d818ba52
Geovani S. Junior <geovanij3@gmail.com>
2021-11-04 10:34:27 -0300

    Add actix and serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| App::new().service(hello))
+        .bind("localhost:8000")?
+        .run();
+
+    println!("Server running at http://{}/", "localhost:8000");
+
+    server.await
+}
+
+#[get("/")]
+async fn hello() -> HttpResponse {
+    HttpResponse::Ok().body("Hello World")
 }
